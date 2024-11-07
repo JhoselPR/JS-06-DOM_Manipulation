137e290e42b0777400b14a39145c3034adb18c1f
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2024-11-07 17:25:26 -0600

    Functions

--- main.js
+++ main.js
@@ -38,43 +38,68 @@ const users = [
         }
     }
 ]
-
+//Constantes globales
 const IMAGE_URL = 'https://placehold.co/200x200';
 const CARD_CONTAINER = document.getElementById('container');
 
+//Botón show/hide
 document.getElementById('btn-hide').addEventListener('click', e => {
     CARD_CONTAINER.style.visibility = (CARD_CONTAINER.style.visibility === 'hidden') ? 'visible' : 'hidden';
 });
 
-const cardCreator = (user) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
+//Creador de tarjetas, dividiendo cada parte de la tarjeta en una función
+const imageCreator = () => {
     const img_container = document.createElement('div');
-
     const image = document.createElement('img');
     image.src = IMAGE_URL;
     image.alt = 'Placeholder user photo';
+    img_container.appendChild(image);
+    return img_container;
+}
 
+const usernameCreator = (user) => {
     const userName = document.createElement('h3');
-    const description = document.createElement('p');
-    const age = document.createElement('p');
-    const bands = document.createElement('ul');
+    userName.textContent = user.username;
+    return userName;
+}
 
+const descriptionCreator = (user) => {
+    const description = document.createElement('p');
+    description.textContent = user.description;
+    return description;
+}
+
+const ageCreator = (user) => {
+    const age = document.createElement('p');
+    age.textContent = user.age;
+    return age;
+}
+
+const bandsCreator = (user) => {
+    const bands = document.createElement('ul');
     const bandList = user.fav_music.bands.map((e) => {
         const item = document.createElement('li');
         item.textContent = e;
         return item;
     });
-    
-    userName.textContent = user.username;
-    description.textContent = user.description;
-    age.textContent = user.age;
-    img_container.appendChild(image);
     bands.append(...bandList);
+    return bands;
+}
+
+//Unifica las partes
+const cardCreator = (user) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const img_container = imageCreator();
+    const userName = usernameCreator(user);
+    const description = descriptionCreator(user);
+    const age = ageCreator(user);
+    const bands = bandsCreator(user);
     card.append(img_container, userName, description, age, bands);
     return card;
 }
 
+//Crea una tarjeta para cada elemento del array
 const cardFiller = () => {
     users.forEach((user) => {
         const card = cardCreator(user);
@@ -84,6 +109,34 @@ const cardFiller = () => {
 cardFiller();
 
 
+// const cardCreator = (user) => {
+//     const card = document.createElement('div');
+//     card.classList.add('card');
+//     const img_container = document.createElement('div');
+
+//     const image = document.createElement('img');
+//     image.src = IMAGE_URL;
+//     image.alt = 'Placeholder user photo';
+
+//     const userName = document.createElement('h3');
+//     const description = document.createElement('p');
+//     const age = document.createElement('p');
+//     const bands = document.createElement('ul');
+
+//     const bandList = user.fav_music.bands.map((e) => {
+//         const item = document.createElement('li');
+//         item.textContent = e;
+//         return item;
+//     });
+    
+//     userName.textContent = user.username;
+//     description.textContent = user.description;
+//     age.textContent = user.age;
+//     img_container.appendChild(image);
+//     bands.append(...bandList);
+//     card.append(img_container, userName, description, age, bands);
+//     return card;
+// }
 
 
 
